ecdc2571a057107f8157c809e279c7e034939568
danial <danialasaria@gmail.com>
2024-03-22 18:26:09 +0300

    caching all files to run without internet

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-A chess engine using Alpha-beta pruning.
+A chess engine powered by vite using Alpha-beta pruning.
+
+To start the development server run:
+```bash
+npm run dev
+```
+
+To create a production build run:
+```bash
+npm run build
+```
--- index.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="https://cdn.rawgit.com/lhartikk/simple-chess-ai/master/lib/js/chess.js"></script>
-<script type="text/javascript" src="https://chessboardjs.com/js/chessboard.js"></script>
-
-var board,
-    game = new Chess();
